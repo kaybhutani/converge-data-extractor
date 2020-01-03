e5aa01d5d26300b42a7de21f7957948f40c13980
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-01-04 03:20:48 +0530

    error msg on no registrations

--- generate.py
+++ generate.py
@@ -2,5 +2,8 @@ import pandas as pd
 
 def generate(data):
   for i in data:
-    dataFrame = pd.DataFrame(data[i])
-    dataFrame.to_excel("./static/temp/"+i+".xlsx")
+    try:
+      dataFrame = pd.DataFrame(data[i])
+      dataFrame.to_excel("./static/temp/"+i+".xlsx")
+    except:
+      print("excel not generated for" + i)
