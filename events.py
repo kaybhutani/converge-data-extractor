538b2d13405024cad7144210bc786709d0fff4b2
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-01-13 08:21:30 +0530

    Update events.py

--- events.py
+++ events.py
@@ -1,6 +1,6 @@
 import pymongo
 import config
-names = {
+names={
     "groove": "GROOVE",
     "inquizzitive": "INQUIZZITIVE",
     "rihaa": "RIHAA",
@@ -11,11 +11,23 @@ names = {
     "fts": "FLUTTER THE SHUTTER",
     "poise": "POISE",
     "mconv": "MR and MS CONVERGE",
-    "adsmd":"Adi Shankaracharya Memorial Debate",
-    "jzbaat":"Jazbaat- The Open Mic Show",
+"adsmd":"Adi Shankaracharya Memorial Debate","jzbaat":"Jazbaat- The Open Mic Show",
     "acst":"ACOUSTICA",
     "crsd":"CRUSADE",
-    "marathon":"Marathon"
+"marathon":"Marathon",
+"algoflux":"AlgoFlux",
+"codft":"CodeFight",
+"rpdfire":"RapidFire",
+"bdmntnboys":"Badminton Boys",
+"bdmntngirls":"Badminton Girls",
+"vllyballb":"Volley Ball Boys",
+ "vllyballg":"Volley Ball Girls",
+"football":"Football",
+"tt":"Table Tennis",
+"crckt":"CRICEKT",
+    "bbb":"Basketball Boys",
+    "bbg":"Basketball Girls",
+    "chess":"Chess"
   }
 heads={
     "groove": "pranav3nov@gmail.com",
@@ -28,11 +40,23 @@ heads={
     "fts": "auraphotography.128@gmail.com",
     "poise": "goyal.jahanvi@gmail.com",
     "mconv": "goyal.jahanvi@gmail.com",
-    "adsmd":"princebest3@rediffmail.com",
-    "jzbaat":"princebest3@rediffmail.com",
+"adsmd":"princebest3@rediffmail.com","jzbaat":"princebest3@rediffmail.com",
     "acst":"fortissimojiit128@gmail.com",
     "crsd":"fortissimojiit128@gmail.com",
-    "marathon":"ashitagoel10@gmail.com"
+ "marathon":"ashitagoel10@gmail.com",
+ "algoflux":"rohanmukhija10@gmail.com",
+    "codft":"rohanmukhija10@gmail.com",
+"rpdfire":"rohanmukhija10@gmail.com",
+"bdmntnboys":"kartikeya2998@gmail.com",
+"bdmntngirls":"kartikeya2998@gmail.com",
+"vllyballb":"gauravjiit4@gmail.com",
+"vllyballg":"gauravjiit4@gmail.com",
+"football":"utkarsh4419@yahoo.com",
+"tt":"shivamkhurana1998@gmail.com",
+"crckt":"toyeshsingh32@gmail.com",
+"bbb":"amoghmisra27@gmail.com",
+"bbg":"amoghmisra27@gmail.com",
+"chess":"nayanraj911@gmail.com"
   }
 
 def getEventsOfHead(email):
