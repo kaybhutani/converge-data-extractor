a02b64ced7586b872cb77b6bc8380c8016281227
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-01-13 08:31:03 +0530

    Update check.py

--- check.py
+++ check.py
@@ -8,7 +8,15 @@ data = {
   "fortissimojiit128@gmail.com": "332111",
   "ashitagoel10@gmail.com": "247889",
   "kbhutani0001@gmail.com": "696969",
-"auraphotography.128@gmail.com":"212121"
+"auraphotography.128@gmail.com":"212121",
+"rohanmukhija10@gmail.com": "452001",
+"gauravjiit4@gmail.com": "002751",
+"kartikeya2998@gmail.com": "299834",
+"utkarsh4419@yahoo.com": "441902",
+"shivamkhurana1998@gmail.com": "761998",
+"toyeshsingh32@gmail.com": "320891",
+"amoghmisra27@gmail.com": "270869",
+"nayanraj911@gmail.com": "699110"
 }
 
 def checkPassword(email, password):
